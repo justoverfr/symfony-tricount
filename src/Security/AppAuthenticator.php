f26f3b72b4502e1b2d5b6841249e59fabcf597eb
Dounya <dounya.berramdane@edu.esiee-it.fr>
2023-09-14 10:59:26 +0200

    login

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -50,7 +50,6 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
 
         // For example:
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
         return new RedirectResponse($this->urlGenerator->generate('app_user'));
     }
 
